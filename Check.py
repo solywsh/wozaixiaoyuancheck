1fec98026870f27a5584f1965f7e4b7b21c55f3c
solywsh <someonelikeyouwsh@outlook.com>
2021-01-10 19:30:28 +0800

    first commit

---
+++ Check.py
@@ -0,0 +1,164 @@
+import requests
+import time
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logger.addHandler(console)
+
+mail_host = "smtp.163.com"        # SMTP服务器
+mail_user = "18111180869@163.com" # 用户名
+mail_pass = "QWRCXWKTJBBTDNAE"    # 授权密码，非登录密码 
+sender = '18111180869@163.com'    # 发件人邮箱
+receivers = ['someonelikeyouwsh@outlook.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+title = '我在校园自动打卡提醒'  # 邮件主题
+
+def sendEmail(content):
+    message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
+    message['From'] = "{}".format(sender)
+    message['To'] = ",".join(receivers)
+    message['Subject'] = title
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口一般是465
+        smtpObj.login(mail_user, mail_pass)  # 登录验证
+        smtpObj.sendmail(sender, receivers, message.as_string())  # 发送
+        print("mail has been send successfully.")
+        logger.info("mail has been send successfully.")
+    except smtplib.SMTPException as e:
+        print(e)
+        logger.info(e)
+
+def send_email2(SMTP_host, from_account, from_passwd, to_account, subject, content):
+    email_client = smtplib.SMTP(SMTP_host)
+    email_client.login(from_account, from_passwd)
+    # create msg
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')  # subject
+    msg['From'] = from_account
+    msg['To'] = to_account
+    email_client.sendmail(from_account, to_account, msg.as_string())
+    email_client.quit()    
+    
+    
+def HealthCheckIn():
+    headers = {
+        'content-length' : '296',
+        'cookie' : 'SESSION=NGY4ZGYwNGMtZTQ3ZC00ZDRmLTg2MmEtNDRhMDYyOTZlYTAw;path=/;HttpOnly',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+        'content-type' : 'application/x-www-form-urlencoded',
+        'token' : '3097c659-2b8c-4ffe-8ad7-9edcbc82b480',
+        'refer' : 'https://servicewechat.com/wxce6d08f781975d91/143/page-frame.html',
+        'accept-encoding' : 'gzip, deflate, br'
+    }
+    Hpostdata = {
+        'answers' : '["0","36.5"]',
+        'latitude' : '34.108216',
+        'longitude' : '108.605084',
+        'country' : '中国',
+        'city' : '西安市',
+        'district' : '鄠邑区',
+        'province' : '陕西省',
+        'township' : '甘亭街道',
+        'street' : '东街',
+        'areacode' : '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/health/save.json'
+    s = requests.session()
+    r = s.post(url, data=Hpostdata,headers=headers)
+    t = r.text
+    if t[8] == '0' :
+        sendEmail("晨检/健康打卡打卡成功")
+        logger.info("晨检/健康打卡打卡")
+    else:
+        sendEmail("token链接失效!请尽快重新输入")
+        logger.warning("token失效，或是未登录")
+    
+    
+def MorningCheck():
+    headers = {
+        'content-length' : '306',
+        'cookie' : 'SESSION=NGY4ZGYwNGMtZTQ3ZC00ZDRmLTg2MmEtNDRhMDYyOTZlYTAw;path=/;HttpOnly',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+        'content-type' : 'application/x-www-form-urlencoded',
+        'token' : '3097c659-2b8c-4ffe-8ad7-9edcbc82b480',
+        'refer' : 'https://servicewechat.com/wxce6d08f781975d91/143/page-frame.html',
+        'accept-encoding' : 'gzip, deflate, br'
+    }
+    Mpostdata = {
+        'answers' : '["0"]',
+        'seq' : '1',
+        'temperature' : '36.5',
+        'userid' : '',
+        'latitude' : '34.108216',
+        'longitude' : '108.605084',
+        'country' : '中国',
+        'city' : '西安市',
+        'district' : '鄠邑区',
+        'province' : '陕西省',
+        'township' : '甘亭街道',
+        'street' : '东街',
+        'myArea' : '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=Mpostdata, headers=headers)
+      
+def NoonInspection():
+    headers = {
+        'content-length' : '134',
+        'cookie' : 'SESSION=NGY4ZGYwNGMtZTQ3ZC00ZDRmLTg2MmEtNDRhMDYyOTZlYTAw;path=/;HttpOnly',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+        'content-type' : 'application/x-www-form-urlencoded',
+        'token' : '3097c659-2b8c-4ffe-8ad7-9edcbc82b480',
+        'refer' : 'https://servicewechat.com/wxce6d08f781975d91/143/page-frame.html',
+        'accept-encoding' : 'gzip, deflate, br'
+    }
+    postdata = {
+        'answers' : '["0","36.5"]',
+        'seq' : '2',
+        'temperature' : '36.6',
+        'userid' : '',
+        'latitude' : '34.108216',
+        'longitude' : '108.605084',
+        'country' : '中国',
+        'city' : '西安市',
+        'district' : '鄠邑区',
+        'province' : '陕西省',
+        'township' : '甘亭街道',
+        'street' : '东街',
+        'myArea' : '610118',
+        'areacode' : '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=postdata, headers=headers)
+    if t[8] == '0' :
+        logger.info("午检打卡打卡")
+    else:
+        sendEmail("token链接失效!请尽快重新输入")
+        logger.warning("token失效，或是未登录")
+    
+if __name__ == "__main__":
+    logger.info("Start print log")
+    while True:
+        time_now = time.strftime("%H:%M:%S", time.localtime()) # 刷新
+        if time_now == "06:30:10" or time_now == "06:30:11":#不知道是奇数还是偶数
+            HealthCheckIn()#健康打卡
+            MorningCheck()#晨间
+            subject = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " 晨间打卡/健康"
+            
+        
+        if time_now == "11:20:10" or time_now == "11:20:11":
+            NoonInspection()#午检
+            subject = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + " 午检打卡"
+            
+        time.sleep(2) # 因为以秒定时，所以暂停2秒，使之不会在1秒内执行多次
---
+++ 我在校园午签到.py
@@ -0,0 +1,38 @@
+import requests
+
+def NoonInspection():
+    headers = {
+        'content-length' : '134',
+        'cookie' : 'SESSION=NGY4ZGYwNGMtZTQ3ZC00ZDRmLTg2MmEtNDRhMDYyOTZlYTAw;path=/;HttpOnly',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+        'content-type' : 'application/x-www-form-urlencoded',
+        'token' : 'a8865ccf-5ba4-40e8-8c33-4902e82ac7ef',
+        'refer' : 'https://servicewechat.com/wxce6d08f781975d91/143/page-frame.html',
+        'accept-encoding' : 'gzip, deflate, br'
+    }
+    postdata = {
+        'answers' : '["0","36.5"]',
+        'seq' : '2',
+        'temperature' : '36.6',
+        'userid' : '',
+        'latitude' : '',
+        'longitude' : '108.605084',
+        'country' : '中国',
+        'city' : '西安市',
+        'district' : '鄠邑区',
+        'province' : '陕西省',
+        'township' : '',
+        'street' : '东街',
+        'myArea' : '',
+        'areacode' : '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=postdata, headers=headers)
+    t = r.text
+    print(t[8])
+    if t[8] == '0' :
+        print(t[8])
+
+if __name__ == "__main__":
+    NoonInspection()
