c917cae4cee8e5d80b14a3f9f8008a0ad17df8ba
solywsh <someonelikeyouwsh@outlook.com>
2021-12-23 11:34:10 +0800

    增加多个班级打卡

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 R
 "check_flask/config/id_201907071004,8\1\818e45d91259ec1a0ce5c2f06ffa815cd50a946a
-G
-脚本/requirements.txt,a\c\acf3593aba93fedc9803d511cf58f4b2fbdefc6b
 H
 check_flask/app/Check.py,6\7\671750cdecd04faa568d89d4991d4cf1feb3aebf
 H
@@ -17,9 +15,9 @@ O
 脚本/我在校园午签到.py,0\3\035375082feca4d2f054ce1bcd2c84f0dd998d23
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-L
-脚本/晚上定位签到.py,d\6\d69fc4097879ed19fc01933c7b01a2d4c468d8d7
 S
 #脚本/晚上定位签到_班级.py,2\d\2d29b533fae1cb8fc1fe67cbe6ccc0d6f59136ad
+L
+脚本/晚上定位签到.py,d\6\d69fc4097879ed19fc01933c7b01a2d4c468d8d7
 C
 脚本/check_all.py,5\d\5d718a7be966a145d168547802e6f03416f5bec0
---
+++ 脚本/同时多个班级/check_all_multiple_classes.py
@@ -0,0 +1,552 @@
+# encoding:utf-8
+import datetime
+import json
+import pprint
+import time
+from pathlib import Path
+
+import requests
+
+# import ast #字符转字典 user_dict = ast.literal_eval(user)
+
+user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.16(0x1800103a) NetType/WIFI Language/zh_CN'
+
+
+def readfile(path=r'info.json'):
+    my_file = Path(path)
+    if my_file.is_file():
+        # 指定的文件存在
+        with open(path, 'r', encoding='utf-8') as f:
+            file_contents = json.load(f)
+        f.close()
+        return file_contents
+    else:
+        print('\n文件不存在...')
+        return 0
+
+
+def pushplus_post(title, content, pushplus_token):
+    url = 'http://pushplus.hxtrip.com/send'
+    data = {
+        "token": pushplus_token,
+        "title": title,
+        "content": content
+    }
+    body = json.dumps(data).encode(encoding='utf-8')
+    headers = {'Content-Type': 'application/json'}
+    requests.post(url, data=body, headers=headers)
+
+
+def unchecked_list_health(date, jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'type': '0',
+        'date': date,
+    }
+    url = 'https://student.wozaixiaoyuan.com/health/getHealthUsers.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    ts = json.loads(r.text)  # 转为字典
+    phone_list = []
+    name_list = ''
+    for t in ts['data']:
+        phone_list.append('+86' + t['phone'])
+        name_list = name_list + t['name'] + ' '
+        print(t['name'])
+    pushplus_post("班级健康打卡提醒", "未打卡名单:" + name_list, pushplus_token)
+
+
+def unchecked_list_morning(date, jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'seq': '1',
+        'date': date,
+        'type': '0'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/getHeatUsers.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+
+    ts = json.loads(r.text)  # 转为字典
+    if ts['code'] != -10:
+        phone_list = []
+        name_list = ''
+        for t in ts['data']:
+            phone_list.append('+86' + t['phone'])
+            name_list = name_list + t['name'] + ' '
+            print(t['name'] + ':' + t['userId'])
+            morning_check_for_classmate(t['userId'],jwsession)
+            time.sleep(2)
+        pushplus_post("班级晨检未打卡提醒", "未打卡名单:" + name_list, pushplus_token)
+    else:
+        pushplus_post("班级晨检未打卡提醒", "jwsession可能失效!", pushplus_token)
+
+
+def unchecked_list_afternoon(date, jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'seq': '2',
+        'date': date,
+        'type': '0'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/getHeatUsers.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    ts = json.loads(r.text)  # 转为字典
+    if ts['code'] != -10:
+        phone_list = []
+        name_list = ''
+        for t in ts['data']:
+            phone_list.append('+86' + t['phone'])
+            name_list = name_list + t['name'] + ' '
+            print(t['name'] + ':' + t['userId'] + ' ' + t['number'])
+            afternoon_check_for_classmate(t['userId'],jwsession)
+            time.sleep(2)
+        pushplus_post("班级午检未打卡提醒", "未打卡名单:" + name_list, pushplus_token)
+    else:
+        pushplus_post("班级午检未打卡提醒", "jwsession可能失效!", pushplus_token)
+
+
+def health_check_in(jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'answers': '["0","36.5"]',
+        'latitude': '34.108216',
+        'longitude': '108.605084',
+        'country': '中国',
+        'city': '西安市',
+        'district': '鄠邑区',
+        'province': '陕西省',
+        'township': '甘亭街道',
+        'street': '东街',
+        'areacode': '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/health/save.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        pushplus_post("个人健康打卡打卡提醒", "打卡成功", pushplus_token)
+    else:
+        pushplus_post("个人健康打卡打卡提醒", "打卡失败,返回信息为:{0}".format(str(r_json)), pushplus_token)
+
+
+def morning_check(jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'answers': '["0"]',
+        'seq': '1',
+        'temperature': '36.5',
+        'userid': '',
+        'latitude': '34.108216',
+        'longitude': '108.605084',
+        'country': '中国',
+        'city': '西安市',
+        'district': '鄠邑区',
+        'province': '陕西省',
+        'township': '甘亭街道',
+        'street': '东街',
+        'myArea': '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    # 经过测试，t返回的字典里会有一个状态码，登陆成功为0，不成功为-10，对应的就是第8个字符。
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        pushplus_post("个人晨检提醒", "打卡成功", pushplus_token)
+    else:
+        pushplus_post("个人晨检提醒", "打卡失败,返回信息为:" + str(r_json), pushplus_token)
+
+
+def noon_inspection(jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = {
+        'answers': '["0","36.5"]',
+        'seq': '2',
+        'temperature': '36.6',
+        'userid': '',
+        'latitude': '34.108216',
+        'longitude': '108.605084',
+        'country': '中国',
+        'city': '西安市',
+        'district': '鄠邑区',
+        'province': '陕西省',
+        'township': '甘亭街道',
+        'street': '东街',
+        'myArea': '610118',
+        'areacode': '610118'
+    }
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        pushplus_post("个人午检打卡提醒", "打卡成功", pushplus_token)
+    else:
+        pushplus_post("个人午检打卡提醒", "打卡失败,返回信息为:" + str(r_json), pushplus_token)
+
+
+def morning_check_for_classmate(userid, jwsession):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = "answers=%5B%220%22%5D&seq=1&temperature=36.5&userId=" + userid + "&latitude=&longitude=&country=%E4%B8%AD%E5%9B%BD&city=%E8%A5%BF%E5%AE%89%E5%B8%82&district=%E9%84%A0%E9%82%91%E5%8C%BA&province=%E9%99%95%E8%A5%BF%E7%9C%81&township=&street=&myArea=&areacode="
+
+    # hpostdata = {
+    #     'answers': '["0"]',
+    #     'seq': '2',
+    #     'temperature': '36.5',
+    #     'userid': userid,
+    #     'latitude': '',
+    #     'longitude': '',
+    #     'country': '中国',
+    #     'city': '西安市',
+    #     'district': '鄠邑区',
+    #     'province': '陕西省',
+    #     'township': '',
+    #     'street': '',
+    #     'myArea': '',
+    #     'areacode': ''
+    # }
+
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    print(r.text)
+
+
+def afternoon_check_for_classmate(userid, jwsession):
+    headers = {
+        "jwsession": jwsession,
+        'user-agent': user_agent
+    }
+    hpostdata = "answers=%5B%220%22%5D&seq=2&temperature=36.5&userId=" + userid + "&latitude=&longitude=&country=%E4%B8%AD%E5%9B%BD&city=%E8%A5%BF%E5%AE%89%E5%B8%82&district=%E9%84%A0%E9%82%91%E5%8C%BA&province=%E9%99%95%E8%A5%BF%E7%9C%81&township=&street=&myArea=&areacode="
+
+    # hpostdata = {
+    #     'answers' : '["0"]',
+    #     'seq' : '2',
+    #     'temperature' : '36.5',
+    #     'userid' : userid,
+    #     'latitude' : '',
+    #     'longitude' : '',
+    #     'country' : '中国',
+    #     'city' : '西安市',
+    #     'district' : '鄠邑区',
+    #     'province' : '陕西省',
+    #     'township' : '',
+    #     'street' : '',
+    #     'myArea' : '',
+    #     'areacode' : ''
+    # }
+
+    url = 'https://student.wozaixiaoyuan.com/heat/save.json'
+    s = requests.session()
+    r = s.post(url, data=hpostdata, headers=headers)
+    print(r.text)
+
+
+##############################################################
+# 晚上定位签到
+def get_sign_message(jwsession, pushplus_token):
+    headers = {
+        "jwsession": jwsession
+    }
+    post_data = {
+        "page": 1,
+        "size": 5
+    }
+    url = "https://student.wozaixiaoyuan.com/sign/getSignMessage.json"
+    s = requests.session()
+    r = s.post(url, data=post_data, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        return r_json['data'][0]
+    else:
+        pushplus_post("签到提醒", "jwsession失效!", pushplus_token)
+        return 404
+
+
+def do_sign(sign_message, jwsession, pushplus_token):
+    if sign_message == 404:
+        return 404
+    headers = {
+        "jwsession": jwsession
+    }
+    post_data = {
+        "signId": str(sign_message['id']),
+        "city": "西安市",
+        "id": str(sign_message['logId']),
+        "latitude": '34.10133361816406',
+        "longitude": '108.65825653076172',
+        "country": "中国",
+        "district": "鄠邑区",
+        "township": "五竹街道",
+        "province": "陕西省"
+    }
+
+    url = "https://student.wozaixiaoyuan.com/sign/doSign.json"
+    s = requests.session()
+    r = s.post(url, data=json.dumps(post_data), headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        pushplus_post("签到提醒", "签到成功", pushplus_token)
+    else:
+        pushplus_post("签到提醒", "签到失败,返回信息为:" + str(r_json), pushplus_token)
+
+
+def contrast_date(sign_message):
+    # 得到签到的日期和时间
+    sign_date_str = str(sign_message['start']).split(" ")[0]
+    sign_time_str_start = str(sign_message['start']).split(" ")[1]
+    sign_time_str_end = str(sign_message['end']).split(" ")[1]
+
+    # 得到系统的日期和时间
+    sys_time_info = datetime.datetime.now()
+    sys_date_now = sys_time_info.date()
+    sys_time_now = time.strftime("%H:%M", time.localtime())
+
+    # 判断打卡的日期和今天的日期是否相同
+    if str(sys_date_now) == sign_date_str:
+        # 判断系统时间是否在打卡时间区间
+        if sign_time_str_start <= sys_time_now <= sign_time_str_end:
+            return 0
+        elif sys_time_now <= sign_time_str_start:
+            return -2
+        elif sys_time_now >= sign_time_str_end:
+            return -3
+    else:
+        return -1
+
+
+##################################
+# 班级晚上定位签到
+
+# 得到签到列表（此列表为没有）
+def get_list(jwsession):
+    headers = {
+        "jwsession": jwsession
+    }
+    post_data = {
+        "keyword": "",
+        "page": 1
+    }
+    url = "https://student.wozaixiaoyuan.com/gradeManage/sign/getList.json"
+    s = requests.session()
+    r = s.post(url, data=post_data, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        # pprint.pprint(r_json)
+        return r_json['data'][0]
+    else:
+        return 404
+
+
+# 对比签到时间
+def contrast_date_class(sign_message):
+    # 得到签到的日期和时间
+    if sign_message == 404:
+        return 404, 0
+
+    sign_date_str = str(sign_message['start']).split(" ")[0]
+    sign_time_str_start = str(sign_message['start']).split(" ")[1]
+    sign_time_str_end = str(sign_message['end']).split(" ")[1]
+
+    # 得到系统的日期和时间
+    sys_time_info = datetime.datetime.now()
+    sys_date_now = sys_time_info.date()
+    sys_time_now = time.strftime("%H:%M", time.localtime())
+
+    # 判断打卡的日期和今天的日期是否相同
+    if str(sys_date_now) == sign_date_str:
+        # 判断系统时间是否在打卡时间区间
+        if sign_time_str_start <= sys_time_now <= sign_time_str_end:
+            return 0, sign_message['id']
+        elif sys_time_now <= sign_time_str_start:
+            return -2, 0
+        elif sys_time_now >= sign_time_str_end:
+            return -3, 0
+    else:
+        return -1, 0
+
+
+# 返回未签到名单
+def get_sign_result(sign_id, jwsession):
+    headers = {
+        "jwsession": jwsession
+    }
+    post_data = {
+        "id": sign_id
+    }
+    url = "https://student.wozaixiaoyuan.com/gradeManage/sign/getSignResult.json"
+    s = requests.session()
+    r = s.post(url, data=post_data, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        pprint.pprint(r_json['data']["notSign"])
+        return r_json['data']["notSign"]
+    else:
+        return 404
+
+
+def do_sign_class(stu_sign_id, jwsession):
+    headers = {
+        "jwsession": jwsession
+    }
+    post_data = {
+        "id": stu_sign_id,
+        "type": 1
+    }
+    url = "https://student.wozaixiaoyuan.com/gradeManage/sign/leaderSign.json"
+    s = requests.session()
+    r = s.post(url, data=post_data, headers=headers)
+    r_json = json.loads(r.text)
+    if r_json['code'] == 0:
+        return 0
+    else:
+        return 404
+
+
+def leader_sign(jwsession, not_sign_list, flag=1):
+    # 设立标志位用于区分周末，如果周末就不打卡，只是返回名单
+    name_list = []
+    for not_sign in not_sign_list:
+        if flag == 1:
+            do_sign_class(not_sign['id'], jwsession)
+        name_list.append(not_sign['name'])
+    return name_list
+
+
+# 打卡事件操作
+def operate_event(jwsession, pushplus_token, flag=1):
+    # 设立标志位用于区分周末，如果周末就不打卡，只是返回名单
+    while True:
+        res, sign_id = contrast_date_class(get_list(jwsession))
+        if res == 0:
+            not_sign_list = get_sign_result(sign_id, jwsession)
+            if not_sign_list != 404:
+                not_sign_name = leader_sign(jwsession, not_sign_list, flag)
+                if len(not_sign_name) != 0:
+                    name_str = ""
+                    for name in not_sign_name:
+                        name_str = name_str + name + " "
+                    pushplus_post("今日班级打卡情况", "未打卡名单:" + name_str, pushplus_token)
+                else:
+                    pushplus_post("今日班级打卡情况", "已经全部打卡", pushplus_token)
+                time.sleep(10)
+                break
+        # 签到是今天但是签到没有开始，静默等待
+        elif res == -2:
+            time.sleep(10)
+        elif res == -3:
+            pushplus_post("今日班级打卡情况", "已过签到时间", pushplus_token)
+            break
+        elif res == -1:
+            pushplus_post("今日班级打卡情况", "签到未发布或今天没有签到", pushplus_token)
+            break
+
+
+def main():
+    while True:
+        # 刷新时间
+        time_now = time.strftime("%H:%M:%S", time.localtime())
+
+        # 晨检
+        if time_now == "06:30:10" or time_now == "06:30:11":
+            infos = readfile()
+            for info in infos:
+                print("正在给{}的{}晨检".format(info["class"], info["name"]))
+                morning_check(info["jwsession"], info["pushplus_token"])
+        # 午检
+        if time_now == "11:05:00" or time_now == "11:05:01":
+            infos = readfile()
+            for info in infos:
+                print("正在给{}的{}午检".format(info["class"], info["name"]))
+                noon_inspection(info["jwsession"], info["pushplus_token"])
+
+        # 晨检未打卡名单并提醒
+        if time_now == "09:40:00" or time_now == "09:40:01":
+            time_data = time.strftime("%Y%m%d")
+            infos = readfile()
+            for info in infos:
+                print("正在给{}的{}班级晨检".format(info["class"], info["name"]))
+                unchecked_list_morning(time_data, info["jwsession"], info["pushplus_token"])  # 检查两次，会出现第一次打卡不完整的情况
+
+        # 午检未打卡名单并提醒
+        if time_now == "14:40:00" or time_now == "14:40:01":
+            time_data = time.strftime("%Y%m%d")
+            infos = readfile()
+            for info in infos:
+                print("正在给{}的{}班级午检".format(info["class"], info["name"]))
+                unchecked_list_afternoon(time_data, info["jwsession"], info["pushplus_token"])
+
+        # 晚上定位签到
+        if time_now == "21:40:00" or time_now == "21:40:01":
+            # 得到最新的签到信息
+            infos = readfile()
+            for info in infos:
+                print("正在给{}的{}班级午检".format(info["class"], info["name"]))
+                sign_info = get_sign_message(info["jwsession"], info["pushplus_token"])
+                if sign_info == 404:
+                    time.sleep(4)
+                    continue
+                # 比对签到信息
+                time_code = contrast_date(sign_info)
+                if time_code == 0:
+                    do_sign(sign_info, info["jwsession"], info["pushplus_token"])
+                    time.sleep(10)
+                elif time_code == -2:
+                    # 签到是今天但是签到没有开始，静默等待
+                    while time_code == 0:
+                        time.sleep(10)
+                        time_code = contrast_date(sign_info)
+                    # 时间开始之后执行签到
+                    do_sign(sign_info, info["jwsession"], info["pushplus_token"])
+                    time.sleep(10)
+                elif time_code == -3:
+                    pushplus_post("签到提醒", "已过签到时间", info["pushplus_token"])
+                elif time_code == -1:
+                    pushplus_post("签到提醒", "签到未发布或今天没有签到", info["pushplus_token"])
+
+        # 班级晚上定位签到
+        week_day_now = time.strftime("%w", time.localtime())
+        # 排除星期天
+        if week_day_now != "0":
+            time_now = time.strftime("%H:%M:%S", time.localtime())
+            if time_now == "22:40:00" or time_now == "22:40:01":
+                infos = readfile()
+                for info in infos:
+                    print("正在给{}的{}班级午检".format(info["class"], info["name"]))
+                    operate_event(info["jwsession"], info["pushplus_token"])  # 打卡
+        # 星期天的情况下
+        else:
+            time_now = time.strftime("%H:%M:%S", time.localtime())
+            if time_now == "22:40:00" or time_now == "22:40:01":
+                infos = readfile()
+                for info in infos:
+                    print("正在给{}的{}班级午检".format(info["class"], info["name"]))
+                    operate_event(info["jwsession"], info["pushplus_token"], 2)  # 返回名单不打卡
+        time.sleep(2)  # 停两秒
+
+
+if __name__ == "__main__":
+    main()
